7cacae956e42609d3c9ee28b2cda872b07091e59
Rayy Benhin <bbenhin@gmail.com>
2017-11-18 03:50:17 +0000

    Add styles

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <div class="navbar-items navbar-start">
+          <nuxt-link class="navbar-item item" to="/">Home</nuxt-link>
+          <span class="navbar-item">|</span>
+          <nuxt-link class="navbar-item item" to="/projects">Projects</nuxt-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+div > .navbar-items {
+  font-size: 1.2rem;
+  display: inline-block;
+}
+.item:hover {
+  text-decoration: underline;
+  color: inherit;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,22 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
       <h1 class="title">
-        portfolio
+        Rayy Benhin
       </h1>
       <h2 class="subtitle">
-        Personal portfolio built with Nuxt.js
+        Software Developer proficient in HTML/CSS, VueJS, NodeJS
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+h2 {
+  font-size: 1.5rem;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+h1, h2 {
+  line-height: 2;
+  font-weight: 400;
 }
 </style>
